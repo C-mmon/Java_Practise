a0f55b667abe6f4e64555b2fa67e9f4f9e1c6fb7
Aniket Kumar <aniket.kumar@cerner.com>
2022-01-20 01:35:25 +0530

    java_and_leetcode_practise

---
+++ static_test.java
@@ -0,0 +1,14 @@
+public class static_test {  
+  
+    public static char display()  
+    {  
+          
+        return 'a';  
+    }  
+      
+    public static void main(String[] args) {  
+  
+        System.out.println(display());  
+  
+    }  
+}
