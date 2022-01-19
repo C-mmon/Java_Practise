57d6189594d53810e42390ae06f0a92d7ec54f14
Aniket Kumar <aniket.kumar@cerner.com>
2022-01-19 11:21:13 +0530

    Added Array List

---
+++ Array_List.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class Array_List
+{
+    public static void main(String [] args)
+    {
+        ArrayList <String> items=new ArrayList<>();
+        items.add("Hair Dryer");
+        items.add("Soap");
+        System.out.println(items);
+    }
+}
